62aaf8af07502b6c4e01f934442e78e14f2cbed1
Eliel <elielasribeiro@gmail.com>
2026-02-08 18:41:26 -0300

    logica da visualnovel 50% pronta

--- scripts/step4.js
+++ scripts/step4.js
@@ -1,36 +1,70 @@
 let currentlyRoute = null;
 
+const dialogue = document.getElementById("Dialogue");
 const divs = document.querySelectorAll(".text");
-//habilitar uma div em especifico
-function ableDiv(name){
-    for (let div of divs) {
-        if (div.id === name + "Text"){
-            div.style.display = "block";
+const reset = document.getElementById("reset");
+
+function ChangeDialogue(div) {
+  const elements = Array.from(div.children);
+  let index = 0;
+
+  function showNext() {
+    if (index >= elements.length) return;
+
+    dialogue.innerHTML = "";
+
+    const nameH2 = document.createElement("h2");
+    nameH2.id = "Name";
+    nameH2.textContent = "OI"
+    dialogue.appendChild(nameH2);
+
+    const el = elements[index].cloneNode(true);
+    dialogue.appendChild(el);
+    
+
+    if (el.tagName === "P") {
+        if (elements[index + 1]?.tagName === "DIV"){
+
+            const btnDiv = elements[index + 1];
+            el.addEventListener("click", () => showChooses(btnDiv), { once: true });
         }else{
-            div.style.display = "none";
+            el.addEventListener("click", showNext, { once: true });
         }
     }
+
+    index++;
+  }
+
+  showNext();
 }
-//por padrao a inicial
-ableDiv("initial");
 
-//resetar para a div inicial
-const resetToInital = document.getElementById("reset");
-resetToInital.style.display = "none";
+function showChooses(btnDiv){
 
-resetToInital.addEventListener("click", () => {
-    currentlyRoute = null;
-    ableDiv("initial");
-    resetToInital.style.display = "none";
-})
+    const buttons = Array.from(btnDiv.children);
 
-//pego cada um dos botoes e, quando clicados, habilita aquela div
-const firstChoose = document.querySelectorAll(".FirstChoose");
-firstChoose.forEach((btn) => {
+    buttons.forEach(button => {
 
-    btn.addEventListener("click", () => {
-        currentlyRoute = btn.value;
-        ableDiv(currentlyRoute);
-        resetToInital.style.display = "block";
-    });
-}) 
+        const clone = button.cloneNode(true);
+        dialogue.appendChild(clone);
+
+        clone.addEventListener("click", () => {
+            reset.style.display = "block";
+            currentlyRoute = clone.value;
+            dialogue.innerHTML = "";
+            ChangeDialogue(document.getElementById(clone.value + "Text"));
+        });
+    })
+}
+
+ChangeDialogue(document.getElementById("initialText"));
+
+
+function resetToInitial() {
+  dialogue.innerHTML = "";
+  currentlyRoute = null;
+  reset.style.display = "none";
+  ChangeDialogue(document.getElementById("initialText"));
+}
+
+
+reset.addEventListener("click", resetToInitial);
